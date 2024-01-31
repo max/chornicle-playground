8d31b95ec35bdd660c29ddb0044d9eab8cd88b0b
Max Schoening <max@max.dev>
2024-01-31 14:13:43 -0800

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 // import EditorLayoutExperiment from './components/EditorLayoutExperiment'
-import PullRequestExperiment from './components/PullRequestExperiment'
+// import PullRequestExperiment from './components/PullRequestExperiment'
+import HistoryFilterExperiment from './components/HistoryFilterExperiment'
 
 function App() {
   // return <EditorLayoutExperiment />
-  return <PullRequestExperiment />
+  // return <PullRequestExperiment />
+  return <HistoryFilterExperiment />
 }
 
 export default App
