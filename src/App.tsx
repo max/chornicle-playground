2a41aa9bea70d09a589e0d55b8ad6dd79829a558
Max Schoening <max@max.dev>
2024-02-20 10:55:47 -0800

    Add space for chat sidebar experiment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 // import EditorLayoutExperiment from './components/EditorLayoutExperiment'
 // import PullRequestExperiment from './components/PullRequestExperiment'
-import HistoryFilterExperiment from './components/HistoryFilterExperiment'
+// import HistoryFilterExperiment from './components/HistoryFilterExperiment'
+import ChatViewExperiment from './components/ChatViewExperiment.tsx'
 
 function App() {
   // return <EditorLayoutExperiment />
   // return <PullRequestExperiment />
-  return <HistoryFilterExperiment />
+  // return <HistoryFilterExperiment />
+
+  return <ChatViewExperiment />
 }
 
 export default App
---
+++ src/components/ChatViewExperiment.tsx
@@ -0,0 +1,5 @@
+function ChatViewExperiment() {
+  return null
+}
+
+export default ChatViewExperiment
